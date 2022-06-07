52d34d92319b10fc179c24a9719809bb06857be6
Taby <forwardpivot@proton.me>
2022-06-07 14:50:40 -0500

    Changed the red square toggle to use classList.toggle.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const yellow = document.querySelector('#yellow');
 const blue = document.querySelector('#blue');
 const orange = document.querySelector('#orange');
 
-const redChange = document.querySelector('#redChange');
+const redChange = document.querySelector('.redChange');
 const yellowChange = document.querySelector('#yellowChange');
 const blueChange = document.querySelector('#blueChange');
 const orangeChange = document.querySelector('#orangeChange');
@@ -25,13 +25,17 @@ orange.addEventListener('click', () => {
     alert("You clicked on the orange square");
 });
 
-redChange.addEventListener('click', function() {
-    toggle = !toggle;
-    if(toggle) {
-        (redChange.src = "img/red.jpeg"); 
-    } else {
+redChange.addEventListener('click', () => {
+    // toggle = !toggle;
+    // if(toggle) {
+    //     (redChange.src = "img/red.jpeg"); 
+    // } else {
+    //     redChange.src = "img/blue.jpeg";
+    // }
+    if (redChange.classList.toggle('red')) {
         redChange.src = "img/blue.jpeg";
+    } else {
+        redChange.src = "img/red.jpeg";
     }
-
-});
+})
 
